c528ee93683f0514c78001c33013bd97d570e948
Lex Emmens <info@lexemmens.nl>
2020-05-23 13:46:08 +0200

    Added additional unit tests to verify behavior

--- src/test/java/nl/lexemmens/podman/SaveMojoTest.java
+++ src/test/java/nl/lexemmens/podman/SaveMojoTest.java
@@ -1,12 +1,8 @@
 package nl.lexemmens.podman;
 
 import nl.lexemmens.podman.enumeration.TlsVerify;
-import nl.lexemmens.podman.service.CommandExecutorService;
-import nl.lexemmens.podman.service.ServiceHub;
-import nl.lexemmens.podman.service.ServiceHubFactory;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.logging.Log;
-import org.apache.maven.project.MavenProject;
 import org.apache.maven.settings.Settings;
 import org.apache.maven.settings.crypto.SettingsDecrypter;
 import org.apache.maven.shared.filtering.MavenFileFilter;
@@ -16,7 +12,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -24,51 +19,21 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.io.File;
 
 import static org.mockito.ArgumentMatchers.isA;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 @RunWith(MockitoJUnitRunner.class)
-public class SaveMojoTest {
-
-    @Mock
-    private Settings settings;
-
-    @Mock
-    private CommandExecutorService commandExecutorService;
-
-    @Mock
-    private MavenProject mavenProject;
-
-    @Mock
-    private MavenFileFilter mavenFileFilter;
-
-    @Mock
-    private ServiceHubFactory serviceHubFactory;
-
-    @Mock
-    private ServiceHub serviceHub;
-
-    @Mock
-    private SettingsDecrypter settingsDecrypter;
-
-    @Mock
-    private Log log;
+public class SaveMojoTest extends AbstractMojoTest {
 
     @InjectMocks
     private SaveMojo saveMojo;
-
-    @Before
-    public void setup() {
-
-    }
 
     @Test
     public void testSkipAllActions() throws MojoExecutionException {
         saveMojo.skip = true;
         saveMojo.execute();
 
-        verify(log, Mockito.times(1)).info(Mockito.eq("Podman actions are skipped."));
+        verify(log, times(1)).info(Mockito.eq("Podman actions are skipped."));
     }
 
     @Test
@@ -77,8 +42,8 @@ public class SaveMojoTest {
         saveMojo.skipSave = true;
         saveMojo.execute();
 
-        verify(log, Mockito.times(1)).info(Mockito.eq("Registry authentication is skipped."));
-        verify(log, Mockito.times(1)).info(Mockito.eq("Saving container images is skipped."));
+        verify(log, times(1)).info(Mockito.eq("Registry authentication is skipped."));
+        verify(log, times(1)).info(Mockito.eq("Saving container images is skipped."));
     }
 
     @Test
@@ -89,8 +54,8 @@ public class SaveMojoTest {
 
         Assertions.assertThrows(MojoExecutionException.class, saveMojo::execute);
 
-        verify(log, Mockito.times(1)).info(Mockito.eq("Registry authentication is skipped."));
-        verify(log, Mockito.times(0)).info(Mockito.eq("Saving container images is skipped."));
+        verify(log, times(1)).info(Mockito.eq("Registry authentication is skipped."));
+        verify(log, times(0)).info(Mockito.eq("Saving container images is skipped."));
     }
 
     @Test
@@ -109,8 +74,24 @@ public class SaveMojoTest {
 
         Assertions.assertDoesNotThrow(saveMojo::execute);
 
-        verify(log, Mockito.times(1)).info(Mockito.eq("Registry authentication is skipped."));
-        verify(log, Mockito.times(0)).info(Mockito.eq("Saving container images is skipped."));
+        verify(log, times(1)).info(Mockito.eq("Registry authentication is skipped."));
+        verify(log, times(0)).info(Mockito.eq("Saving container images is skipped."));
+
+        File targetTestPodmanDir = new File("./target/podman-test/podman");
+        verify(commandExecutorService, times(1)).runCommand(new File(targetTestPodmanDir.getAbsolutePath()),
+                "podman",
+                "save",
+                "",
+                "--format",
+                "oci-archive",
+                "-o",
+                "sampleTag_1_0_0.tar.gz",
+                "registry.example.com/sampleTag:1.0.0");
     }
 
+    private String normaliseImageName(String fullImageName) {
+        String[] imageNameParts = fullImageName.split("\\/");
+        String tagAndVersion = imageNameParts[imageNameParts.length - 1];
+        return tagAndVersion.replaceAll("[\\.\\/\\-\\*:]", "_");
+    }
 }
