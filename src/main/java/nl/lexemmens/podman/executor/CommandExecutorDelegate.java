fed375f4b09d5d48d487da2a8ae6dc35bb29b16e
Lex Emmens <info@lexemmens.nl>
2020-05-27 20:22:09 +0200

    Updated Javadoc

--- src/main/java/nl/lexemmens/podman/executor/CommandExecutorDelegate.java
+++ src/main/java/nl/lexemmens/podman/executor/CommandExecutorDelegate.java
@@ -5,6 +5,22 @@ import org.zeroturnaround.exec.ProcessExecutor;
 
 import java.util.List;
 
+/**
+ * Interface that allows delegation of command execution to a specific implementation
+ */
 public interface CommandExecutorDelegate {
+
+    /**
+     * <p>
+     * Executes the command as configured in the provided ProcessExecutor.
+     * </p>
+     * <p>
+     * Throws a MojoExecutionException in case command execution fails
+     * </p>
+     *
+     * @param processExecutor The process executor carrying the command to execute
+     * @return A {@link List} of {@link String}s representing the actual output of the command
+     * @throws MojoExecutionException In case execution of the command fails
+     */
     List<String> executeCommand(ProcessExecutor processExecutor) throws MojoExecutionException;
 }
